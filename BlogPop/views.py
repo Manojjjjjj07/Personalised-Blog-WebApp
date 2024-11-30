b1b6c96a6217060fe33a8ecd06a4a09418a3ea52
El_Sombrero <manojaishu265@gmail.com>
2024-11-30 21:43:23 +0530

    Started Frontend

--- BlogPop/views.py
+++ BlogPop/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'Author': 'SKM',
+        'Title': 'Blog Post 1',
+        'Content': 'First post content',
+        'Date_Posted': 'November 30, 2024'
+    },
+    {
+        'Author': 'Suz',
+        'Title': 'Blog Post 2',
+        'Content': 'Second post content',
+        'Date_Posted': 'November 30, 2024'
+    }
+]
 
 def home(request):
-    return render(request, 'BlogPop/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'BlogPop/home.html', context)
 
 def about(request):
-    return render(request, 'BlogPop/about.html')
+    return render(request, 'BlogPop/about.html',  {'title': 'About'})
