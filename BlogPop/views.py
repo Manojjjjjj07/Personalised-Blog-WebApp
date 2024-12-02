228f7a31ed04cbd046cd7409b95bb8341122e797
El_Sombrero <manojaishu265@gmail.com>
2024-12-02 19:40:08 +0530

    Implemented Database and migrations

--- BlogPop/views.py
+++ BlogPop/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'Author': 'SKM',
-        'Title': 'Blog Post 1',
-        'Content': 'First post content',
-        'Date_Posted': 'November 30, 2024'
-    },
-    {
-        'Author': 'Suz',
-        'Title': 'Blog Post 2',
-        'Content': 'Second post content',
-        'Date_Posted': 'November 30, 2024'
-    }
-]
+from .models import Post
 
 def home(request):
-    context = {
-        'posts': posts
-    }
+    context = {'posts': Post.objects.all()}
     return render(request, 'BlogPop/home.html', context)
 
 def about(request):
