560bcb0a254437869d87ca12abb6fc4a798f7941
El_Sombrero <manojaishu265@gmail.com>
2024-12-07 19:36:59 +0530

    Added User Profile and Picture

--- project_skm/urls.py
+++ project_skm/urls.py
@@ -29,3 +29,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('BlogPop.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
